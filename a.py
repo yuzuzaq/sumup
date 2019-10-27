c65c58f739f9dc62e90484723bb6a67bc9363b6c
森川彩音 <aya@morikawaayanenoMacBook-Pro.local>
2019-10-27 23:39:06 +0900

    素数検知器を作りたい

--- a.py
+++ a.py
@@ -2,3 +2,12 @@ n = input("Please Enter Number: ")
 print(n)
 print(int(0.5*int(n)*(int(n)+1)))
 print(int(n)*int(n))
+
+
+for num in range(2, int(n)):
+    if num % 2 == 0:
+        print("偶数", num)
+    if num % 3 == 0:
+        print("ペー", num)
+    continue
+    print("あほ", num)
